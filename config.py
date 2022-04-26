1bdcb3c3fe64c071b545ff2e1ef5a4ebb7cc5c19
Sᴜᴍɪᴛ Yᴀᴅᴀᴠ <91660827+Sumit9969@users.noreply.github.com>
2022-04-27 00:02:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,3 @@
-"""
-Music Player, Telegram Voice Chat Bot
-Copyright (c) 2021-present Asm Safone <https://github.com/AsmSafone>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>
-"""
-
 import os
 from dotenv import load_dotenv
 
@@ -25,19 +7,19 @@ load_dotenv()
 
 class Config:
     def __init__(self) -> None:
-        self.API_ID: str = os.environ.get("API_ID", None)
-        self.API_HASH: str = os.environ.get("API_HASH", None)
-        self.SESSION: str = os.environ.get("SESSION", None)
-        self.BOT_TOKEN: str = os.environ.get("BOT_TOKEN", None)
+        self.API_ID: str = os.environ.get("API_ID", "10581222")
+        self.API_HASH: str = os.environ.get("API_HASH", "3821fef6f828ec26b116c520db18dc70")
+        self.SESSION: str = os.environ.get("SESSION", "AQAN_WYAoDH9cyyD3_Fe3c3KJqVipYvu2gxCw4AS436T76F38fvxnPZ8UzSIQ9F1Uro3cfDeLKsajqKS_E0CcL51IpP6-huREHMatxzqN2AnUsD2yplKzvnOfZ7fq-xU95q5RKGsEbW517MFjcIeEU2-yBikqN3reWVYeJy-K04JTE8CmuzlUaY2Du6bmh8KC6QSW7_CJlk9yegok906Z-OBJNhuxRsjLBGo2iJV9p00bOVlBMkkfoylii_EtfeZJuTwb8tOGYFA-UI0CttvL5Ijy6x10mPEwjtb8HALSd0jyNkrMyf02yg3KKUtshCToTt6ld5SFUy5L_hByn9b3rn7AAAAATbl3U8A")
+        self.BOT_TOKEN: str = os.environ.get("BOT_TOKEN", "5190612090:AAFADwTTVC6PsstylETeFM9PygpORjzMxFg")
         self.SUDOERS: list = [
-            int(id) for id in os.environ.get("SUDOERS", " ").split() if id.isnumeric()
+            int(id) for id in os.environ.get("SUDOERS", " 5009839424 5221911121 5290547167").split() if id.isnumeric()
         ]
         if not self.SESSION or not self.API_ID or not self.API_HASH:
             print("ERROR: SESSION, API_ID and API_HASH is required!")
             quit(0)
         self.SPOTIFY: bool = False
         self.QUALITY: str = os.environ.get("QUALITY", "high").lower()
-        self.PREFIXES: list = os.environ.get("PREFIX", "!").split()
+        self.PREFIXES: list = os.environ.get("PREFIX", "/,.,!").split()
         self.LANGUAGE: str = os.environ.get("LANGUAGE", "en").lower()
         self.STREAM_MODE: str = (
             "audio"
